31f3a80c4d5a9b142c20d2e67c71e283556548a3
NehaMary23 <nehamaryp@gmail.com>
2026-04-23 18:30:45 +0530

    updated

--- app.config.js
+++ app.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "@tanstack/start/config";
+import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  tsr: {
+    appDirectory: "src",
+  },
+  vite: {
+    plugins: [viteTsConfigPaths()],
+  },
   server: {
     preset: "vercel",
+    experimental: {
+      asyncContext: true,
+    },
   },
 });
